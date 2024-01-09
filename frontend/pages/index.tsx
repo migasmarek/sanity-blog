ec3af2a9e582e16942f376c3be052446a73cd216
Marek Migas <migas@nonstop.studio>
2024-01-09 11:16:00 +0100

    ts any type fix

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,20 @@
 import Link from "next/link";
 import groq from "groq";
 import client from "../client";
+import { GetStaticProps } from "next";
 
-const Index = ({posts}) => {
+interface Post {
+  _id: string;
+  title: string;
+  slug: { current: string };
+  publishedAt: string;
+}
+
+interface IndexProps {
+  posts: Post[];
+}
+
+const Index: React.FC<IndexProps> = ({ posts }) => {
   return (
     <div>
       <h1>Welcome to a blog</h1>
@@ -21,7 +33,7 @@ const Index = ({posts}) => {
   )
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps<IndexProps> = async () => {
   const posts = await client.fetch(
     groq`*[_type == "post" && publishedAt < now()] | order(publishedAt desc)`
   )
